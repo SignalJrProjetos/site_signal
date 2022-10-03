c822947e79398efb5974bd2ee4f3180869b2ecd3
gpbezerra <gabrielpbezerra8@gmail.com>
2022-10-03 18:26:54 -0300

    config inicial

---
+++ site_signal/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import { Home } from "./screens/Home";
+
+export const App = () =>{
+	return (
+		<div className="App">
+			<Home/>
+		</div>
+	);
+};
+
+
