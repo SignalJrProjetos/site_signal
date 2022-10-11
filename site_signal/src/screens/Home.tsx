13b69e1ac686d278741b036fdac1ba1df0b8cc60
Signal Projetos <projeto@signaljr.com.br>
2022-10-11 17:33:27 -0300

    adicionando funcionalidade de enviar email

--- site_signal/src/screens/Home.tsx
+++ site_signal/src/screens/Home.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { Contato } from "../components/Contato";
+import styled from "styled-components";
 
 export const Home = () =>{
+
+	const HomeScreen = styled.div`
+		display: flex;
+	`;
 	return (
-		<div className="App">
+		<HomeScreen>
 			<Contato/>
-		</div>
+		</HomeScreen>
 	);
 };
