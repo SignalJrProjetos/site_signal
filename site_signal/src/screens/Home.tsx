dd3218a5030fd592cb23d7e81a1f92ebdc791f6c
Dani Peixoto <daniellepeixo@gmail.com>
2022-11-23 00:27:04 -0300

    feat(Partners): Responsividade

--- site_signal/src/screens/Home.tsx
+++ site_signal/src/screens/Home.tsx
@@ -14,101 +14,19 @@ import facebookIco from "../assets/svg/icon.facebook.svg";
 import instagramIco from "../assets/svg/icon.instagram.svg";
 import linkedinIco from "../assets/svg/icon.linkedin.svg";
 import aboutLogo from "../assets/svg/logo.SignalSymbolBg.svg";
-import UFRRJ from "../assets/imgs/logo.UFRRJPartners.png";
-import Brasil from "../assets/imgs/logo.Brasil JuniorPartners.png";
-import Rio from "../assets/imgs/logo.Rio JuniorPartners.png";
-import TOTVS from "../assets/imgs/logo.TOTVSPartners.png";
-import Americanas from "../assets/imgs/logo.AmericanasPartners.png";
-import Empower from "../assets/imgs/logo.EmpowerPartners.png";
+import UFRRJ from "../assets/svg/logo.UFRRJPartners.svg";
+import Brasil from "../assets/svg/logo.Brasil JuniorPartners.svg";
+import Rio from "../assets/svg/logo.Rio JuniorPartners.svg";
+import TOTVS from "../assets/svg/logo.TOTVSPartners.svg";
+import Americanas from "../assets/svg/logo.AmericanasPartners.svg";
+import Empower from "../assets/svg/logo.EmpowerPartners.svg";
 
 
 export const Home = () =>{
 	
 	return (
 		<div className="App">
-			<ParticlesContainer/>
-			<Navbar/>
-
-			{/* Banner */}
-			<div className="bannerContainer">
-				<div className="bannerTextContainer">
-					<div>
-						<h1>
-								A solução digital ideal para o seu negócio.
-						</h1>
-						<p>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque rhoncus, urna egestas convallis hendrerit, 
-								purus arcu consectetur sapien, eget molestie mi elit sed erat. Curabitur vel ullamcorper odio. Nulla ullamcorper 
-								massa sit amet aliquam elementum. Vestibulum faucibus sem sit amet quam lacinia suscipit. 
-						</p>
-						<br></br>
-						<br></br>
-						<a className="buttonWhite" style={{"fontFamily": "Saira", "fontSize": "28px", "position": "relative"}} href="">Saiba Mais</a>
-					</div>
-					<div>
-						<img className="bannerImg" src={bannerImg}/>
-					</div>
-					<div className="bannerSocialMediaContainer">
-						<p>SIGA-NOS</p>
-						<br></br>
-						<a style={{"marginLeft": "4px"}} href=""><img src={facebookIco}/></a>
-						<br></br>
-						<br></br>
-						<a style={{"marginLeft": "2px"}} href=""><img src={instagramIco}/></a>
-						<br></br>
-						<br></br>
-						<a style={{"marginLeft": "2px"}} href=""><img src={linkedinIco}/></a>
-					</div>
-				</div>
-			</div>
-			<img className="bannerBorder" src={border}/>
-			<Solutions/>
-
-			{/*Sobre Nós*/}
-
-			{
-				<div className="aboutContainer">
-					<div className="aboutTextContainer">
-						<h2>Sobre Nós</h2>
-						<h1 className="title">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut erat sed nisi euismod suscipit. Suspendisse diam magna, 
-							lobortis ac justo vel, auctor dictum magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut erat sed 
-							nisi euismod suscipit.
-						</p>
-						<br/>
-						<a className="buttonPurple2" href="">Saiba Mais</a>
-					</div>
-					<div>
-						<img className="aboutImg" src={aboutLogo}/>
-					</div>
-				</div>
-			}
-
-			<Contact/>
-
-			{/* Depoimentos */}
-			<div className="testimonialsContainer">
-				<div>
-					<h2>Depoimentos</h2>
-					<h1 className="title">O que nossos clientes falam</h1>
-				</div>
-
-				{/* Cada Carousel.Item corresponde a um componente */}
-				<Carousel>
-					<Carousel.Item>
-						<Testimonial/>
-					</Carousel.Item>
-					<Carousel.Item>
-						<Testimonial/>
-					</Carousel.Item>
-					<Carousel.Item>
-						<Testimonial/>
-					</Carousel.Item>
-					<Carousel.Item>
-						<Testimonial/>
-					</Carousel.Item>
-				</Carousel>
-			</div>
+			
 
 			{/* Parceiros*/}
 	
@@ -117,19 +35,15 @@ export const Home = () =>{
 					<h1 className="title">Parceiros</h1>
 				</div>
            
-				<div className="partnersImagesOne">
-					<img className="partnerImg" src={UFRRJ} alt=""/>
-					<img className="partnerImg" src={Brasil} alt=""/>
-					<img className="partnerImg" src={Rio} alt=""/>
-					<img className="partnerImg" src={TOTVS} alt=""/>
+				<div className="partnersImages">
+					<img className="partnerImg" id="UFRRJ" src={UFRRJ} alt=""/>
+					<img className="partnerImg" id="Brasil" src={Brasil} alt=""/>
+					<img className="partnerImg" id="Rio" src={Rio} alt=""/>
+					<img className="partnerImg" id="TOTVS" src={TOTVS} alt=""/>
+					<img className="partnerImg" id="Empower" src={Empower} alt=""/>
+					<img className="partnerImg" id="Americanas" src={Americanas} alt=""/>
 				</div>
-
-				<div className="partnersImagesTwo">
-					<img className="partnerImg" src={Empower} alt=""/>
-					<img className="partnerImg" src={Americanas} alt=""/>
-				</div>
-			</div>
-			<Footer/>
+			</div>	
 		</div>
 	);
 };
