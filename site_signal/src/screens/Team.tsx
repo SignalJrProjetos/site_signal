365bb430f4e9635435fb80136fe2af87e7ef5e56
matt-ritton <ritton.matheus@gmail.com>
2022-12-08 23:20:08 -0300

    feat(Header): componente Header

--- site_signal/src/screens/Team.tsx
+++ site_signal/src/screens/Team.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "../screens/css/team.css";
+import { ParticlesContainer } from "../components/ParticlesContainer";
+import { Navbar } from "../components/Navbar/Navbar";
+import { Header } from "../components/Header/Header";
 
 export const Team = () =>{
 	return (
 		<div className="team">
-			<h1>Página time</h1>
+			<ParticlesContainer/>
+			<Navbar/>
+			<Header 
+				title="Conheça a Signal Júnior e dê o próximo passo com seu negócio"
+				text="Conheça um pouco mais sobre a nossa empresa, o que é o Movimento Empresa Júnior e nossos talentosos membros."
+			/>
 		</div>
 	);
 };
