85d1df43c3d8c8c7e160b617d9a12eafdc012b41
matt-ritton <ritton.matheus@gmail.com>
2022-12-26 19:39:58 -0300

    fix(index + package.json)): correções de acesso ao site

--- site_signal/src/components/AboutUs/AboutUs.tsx
+++ site_signal/src/components/AboutUs/AboutUs.tsx
@@ -13,15 +13,13 @@ export const AboutUs = () => {
 					<img className="aboutImg" src={aboutLogo}/>
 				</div>
 
-				<h2 className="subtitle">Sobre Nós</h2>
-				<h1 className="title">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
+				<h2 className="subtitle">Somos a Signal Jr</h2>
+				<h1 className="title">A única Empresa Júnior de Tecnologia da Informação inovadora do Rio de Janeiro.</h1>
 				<p>
-						Somos a Signal Jr, a única Empresa Júnior de Tecnologia da Informação inovadora do Rio de Janeiro.
-
-						Criada no ano de 2015, a Signal Jr. tem o objetivo de trazer a experiência empreendedora dentro da nossa formação acadêmica. 
-						Formada por uma equipe multidisciplinar, que valoriza sua diversidade nas perspectivas da EJ e na tomada de decisão, além de 
-						dinamizar os processos através de qualificações distintas e diferentes bagagens. Quem faz parte da Signal Jr. carrega consigo 
-						5 valores em comum, que nos norteiam enquanto EJ: Protagonismo, Querência, Resiliência, Level Up e Conexão.
+					Criada no ano de 2015, a Signal Jr. tem o objetivo de trazer a experiência empreendedora dentro da nossa formação acadêmica. 
+					Formada por uma equipe multidisciplinar, que valoriza sua diversidade nas perspectivas da EJ e na tomada de decisão, além de 
+					dinamizar os processos através de qualificações distintas e diferentes bagagens. Quem faz parte da Signal Jr. carrega consigo 
+					5 valores em comum, que nos norteiam enquanto EJ: Protagonismo, Querência, Resiliência, Level Up e Conexão.
 				</p>
 				<br/>
 				<a className="buttonPurple2" href={process.env.PUBLIC_URL + "/developing"}>Saiba Mais</a>
