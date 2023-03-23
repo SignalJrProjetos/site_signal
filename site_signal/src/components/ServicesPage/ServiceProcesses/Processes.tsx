8cfce87dbc64951b5a3689a42ee7d160746f00b9
matt-ritton <ritton.matheus@gmail.com>
2023-03-23 15:31:25 -0300

    style(Processes): timeline versão mobile

--- site_signal/src/components/ServicesPage/ServiceProcesses/Processes.tsx
+++ site_signal/src/components/ServicesPage/ServiceProcesses/Processes.tsx
@@ -6,9 +6,11 @@ export const Processes = (props: any) => {
 
 	const[title, setTitle] = useState<string>(props.stepByStep[0].substring(0, props.stepByStep[0].indexOf(":")));
 	const[text, setText] = useState<string>(props.stepByStep[0].substring(props.stepByStep[0].indexOf(":") + 1, props.stepByStep[0].length));
-	const[active, setActive] = useState<number>(-1);
+	
+	//Estado responsável por controlar qual botão foi clicado. [default(0) = primeiro botão]
+	const[active, setActive] = useState<number>(0);
 
-	//Estado responsável por controlar a exibição de textos após cliques em botões
+	//Estado responsável por controlar a exibição de textos após cliques em botões [default(true) = estado ativo]
 	const[showText, setShowText] = useState<boolean>(true);
 
 	//Verifica se title foi alterado. Se true, ativa um temporizador para definir setShowText para false
@@ -34,38 +36,62 @@ export const Processes = (props: any) => {
 			<h2 style={{"textAlign":"center"}} className="subtitle">Etapas do Serviço</h2>
 			<h1 style={{"textAlign":"center"}} className="title">Clique nos botões abaixo para saber mais sobre cada uma das etapas do serviço.</h1>
 
-			<div className="timeline">
+			
+			{/* Versão Desktop */}
+			<div className="timelineContainer">
+				<div className="timeline">
 
-				{/** Mapeia o array de objetos vindo da prop stepByStep, tanto o índice quanto string **/}
-				{props.stepByStep.map((step: string, index: number) => (
+					{/** Mapeia o array de objetos vindo da prop stepByStep, tanto o índice quanto string **/}
+					{props.stepByStep.map((step: string, index: number) => (
 
-					<div key={index}>
+						<div key={index}>
 
-						{/** 
+							{/** 
 						 * Para cada objeto do array, cria-se um botão.
-						 * Baseado no índice do objeto (par ou ímpar), os botões são posicionados de forma diferente
+						 * Baseado no índice do objeto (par ou ímpar), os botões são posicionados de forma diferente.
+						 * Se for o último índice, aplica uma estilização única.
 						 **/}
 
-						<div className={`buttonContainer ${index % 2 === 0 ? "processButton--above" : "processButton--below"} ${index === props.stepByStep.length - 1 ? "buttonContainer--last" : ""}`}>
-							<button
-								className={index == active ? "processButtonActive" : "processButton"}
-								onClick={() => changeText(step, index)}>
-								{index + 1}
-							</button>
-							<span>{step.substring(0,step.indexOf(":"))}</span>
+							<div className={`buttonContainer ${index % 2 === 0 ? "processButton--above" : "processButton--below"} ${index === props.stepByStep.length - 1 ? "buttonContainer--last" : ""}`}>
+								<button
+									className={index == active ? "processButtonActive" : "processButton"}
+									onClick={() => changeText(step, index)}>
+									{index + 1}
+								</button>
+								<span>{step.substring(0,step.indexOf(":"))}</span>
+							</div>
+
+							<div className="processPoint"></div>
+					
 						</div>
 
-						<div className="processPoint"></div>
-					
-					</div>
+					))}
 
+				
+				</div>
+				<div className="processInfoContainer">
+					<h1 className={showText ? "textFadeIn" : ""}>{title}</h1>
+					<p className={showText ? "textFadeIn" : ""}>{text}</p>
+				</div>
+			</div>
+
+			{/* Versão Mobile */}
+			<div className="timelineMobile">
+				{props.stepByStep.map((step: string) => (
+					// eslint-disable-next-line react/jsx-key
+					<div style={{"display":"flex", "marginBottom": "12px"}}>
+
+						<div className="processLine"></div>
+
+						<div className="processInfoContainer">
+							<h1 className={showText ? "textFadeIn" : ""}>{step.substring(0, step.indexOf(":"))}</h1>
+							<p className={showText ? "textFadeIn" : ""}>{step.substring(step.indexOf(":") + 1, step.length)}</p>
+						</div>
+
+					</div>
 				))}
 			</div>
-
-			<div className="processInfoContainer">
-				<h1 className={showText ? "textFadeIn" : ""}>{title}</h1>
-				<p className={showText ? "textFadeIn" : ""}>{text}</p>
-			</div>
+			
 		</div>
 	);
 };
